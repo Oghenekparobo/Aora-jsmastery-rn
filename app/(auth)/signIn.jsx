28355091e76d2e3089462eb2cc53fdbc9e233577
oghenekparobo <classicsteph11@gmail.com>
2025-01-24 02:44:34 +0100

    fix

--- app/(auth)/signIn.jsx
+++ app/(auth)/signIn.jsx
@@ -1,63 +1,93 @@
-import { View, Text, ScrollView, Image } from "react-native";
-import React, { useState } from "react";
+import { useState } from "react";
+import { Link, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { images } from "@/constants";
-import FormField from "@/components/FormField";
+import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
 import CustomButton from "@/components/CustomButton";
-import { Link } from "expo-router";
+import { useGlobalContext } from "@/context/GlobalProvider";
+import FormField from "@/components/FormField";
+import { images } from "@/constants";
+import { signIn } from "@/lib/appwrite";
 
 const SignIn = () => {
+  const { setUser, setIsLogged } = useGlobalContext();
+  const [isSubmitting, setSubmitting] = useState(false);
   const [form, setForm] = useState({
-    password: "",
     email: "",
+    password: "",
   });
 
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
 
-  const submit = async () => {};
+    setSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+      const result = await getCurrentUser();
+      setUser(result);
+      setIsLogged(true);
+
+      Alert.alert("Success", "User signed in successfully");
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="flex items-center justify-center min-h-[85vh] px-4 my-6">
+        <View
+          className="w-full flex justify-center h-full px-4 my-6"
+          style={{
+            minHeight: Dimensions.get("window").height - 100,
+          }}
+        >
           <Image
             source={images.logo}
             resizeMode="contain"
-            className="w-[115px] h-[35px]"
+            className="w-[115px] h-[34px]"
           />
-          <Text className="text-white text-2xl font-psemibold mt-4">
-            Log in to aora
+
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Log in to Aora
           </Text>
+
           <FormField
-            title="email"
+            title="Email"
             value={form.email}
             handleChangeText={(e) => setForm({ ...form, email: e })}
-            otherStyles="mt-6"
+            otherStyles="mt-7"
             keyboardType="email-address"
           />
 
           <FormField
-            title="password"
+            title="Password"
             value={form.password}
             handleChangeText={(e) => setForm({ ...form, password: e })}
-            otherStyles="mt-6"
+            otherStyles="mt-7"
           />
 
           <CustomButton
-            title="sign in"
+            title="Sign In"
             handlePress={submit}
-            containerStyles={"mt-7"}
+            containerStyles="mt-7"
             isLoading={isSubmitting}
           />
 
-          <View className="flex-row items-center justify-center mt-6">
-            <Text className="text-white text-xl font-pmedium">
-              Don't have an account? {""}
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
             </Text>
             <Link
-              href="/signup"
-              className="text-secondary font-psemibold capitalize text-xl"
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
             >
-              sign up
+              Signup
             </Link>
           </View>
         </View>
